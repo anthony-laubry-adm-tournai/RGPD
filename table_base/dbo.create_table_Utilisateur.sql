aaefe0a1071040a116274471359163469f966a60
anthony-laubry-adm-tournai <39951085+anthony-laubry-adm-tournai@users.noreply.github.com>
2018-06-06 11:18:04 +0200

    Update dbo.create_table_Utilisateur.sql

--- table_base/dbo.create_table_Utilisateur.sql
+++ table_base/dbo.create_table_Utilisateur.sql
@@ -1,10 +1,9 @@
-﻿CREATE TABLE [dbo].[Utilisateur] (
+CREATE TABLE [dbo].[Utilisateur] (
     [Id]          BIGINT       IDENTITY (1, 1) NOT NULL,
     [DisplayName] VARCHAR (50) NOT NULL,
     [LogonName]   VARCHAR (25) NOT NULL,
     [IdService]   BIGINT       NULL,
     [MotPasse]    VARCHAR (75) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
-    FOREIGN KEY ([IdService]) REFERENCES [dbo].[Service] ([Id])
 );
 
