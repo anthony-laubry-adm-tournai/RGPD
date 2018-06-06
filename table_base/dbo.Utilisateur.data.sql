1b064a3aae4744054c9a1850502ab71c06a74859
anthony-laubry-adm-tournai <39951085+anthony-laubry-adm-tournai@users.noreply.github.com>
2018-06-06 15:40:26 +0200

    Add files via upload

---
+++ table_base/dbo.Utilisateur.data.sql
@@ -0,0 +1,3 @@
+SET IDENTITY_INSERT [dbo].[Utilisateur] ON
+INSERT INTO [dbo].[Utilisateur] ([Id], [DisplayName], [LogonName], [IdService], [MotPasse]) VALUES (1, N'Super Admin', N'Administrateur', NULL, N'bJaTaqD4ufj53M2IZqqXOA==')
+SET IDENTITY_INSERT [dbo].[Utilisateur] OFF
